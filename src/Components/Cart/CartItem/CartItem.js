5cc01a8d77ac04d074e8903b6de348bc6ffbf074
ahmedsa11 <ahmedsa122333@gmail.com>
2023-01-06 20:03:55 +0200

    as

--- src/Components/Cart/CartItem/CartItem.js
+++ src/Components/Cart/CartItem/CartItem.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import { Button, Col,Modal,Row } from 'react-bootstrap'
-import mobile from '../../../images/mobile.png'
-import deleteicon from '../../../images/delete.png'
-import './CartItem.css'
-import DeleteCartHook from '../../../Hook/cart/delete-cart-hook'
-const CartItem = ({item}) => {
-  const  [
+import React from "react";
+import { Button, Col, Modal, Row } from "react-bootstrap";
+import mobile from "../../../images/mobile.png";
+import deleteicon from "../../../images/delete.png";
+import "./CartItem.css";
+import DeleteCartHook from "../../../Hook/cart/delete-cart-hook";
+const CartItem = ({ item }) => {
+  const [
     ,
     show,
     handleClose,
@@ -15,32 +15,49 @@ const CartItem = ({item}) => {
     onChangeCount,
     handeleUpdateCart,
     ,
-  ]= DeleteCartHook(item,item.product._id)
-  console.log(item)
-    return (
-      <Col xs="12" className="cart-item-body my-2 d-flex px-2">
-
+  ] = DeleteCartHook(item, item.product._id);
+  console.log(item);
+  return (
+    <Col xs="12" className="cart-item-body my-2 d-flex px-2">
       <Modal show={show} onHide={handleClose}>
-        <Modal.Header >
-          <Modal.Title> <div className='font'>تاكيد الحذف</div></Modal.Title>
+        <Modal.Header>
+          <Modal.Title>
+            {" "}
+            <div className="font">تاكيد الحذف</div>
+          </Modal.Title>
         </Modal.Header>
-        <Modal.Body><div className='font'>هل انتا متاكد من حذف المنتج من العربة</div></Modal.Body>
+        <Modal.Body>
+          <div className="font">هل انتا متاكد من حذف المنتج من العربة</div>
+        </Modal.Body>
         <Modal.Footer>
-          <Button className='font' variant="success" onClick={handleClose}>
+          <Button className="font" variant="success" onClick={handleClose}>
             تراجع
           </Button>
-          <Button className='font' variant="dark" onClick={handelDeleteItem}>
+          <Button className="font" variant="dark" onClick={handelDeleteItem}>
             حذف
           </Button>
         </Modal.Footer>
       </Modal>
 
-      <img width="160px" height="197px" src={`http://127.0.0.1:8000/products/${item.product ? item.product.imageCover : mobile}`} alt="" />
+      <img
+        width="160px"
+        height="197px"
+        src={`https://combative-plum-blackbuck.cyclic.app/${
+          item.product ? item.product.imageCover : mobile
+        }`}
+        alt=""
+      />
       <div className="w-100">
         <Row className="justify-content-between">
           <Col sm="12" className=" d-flex flex-row justify-content-between">
-            <div className="d-inline pt-2 cat-text">{item.product.category?item.product.category.name : ""}</div>
-            <div onClick={handleShow} className="d-flex pt-2 " style={{ cursor: "pointer" }}>
+            <div className="d-inline pt-2 cat-text">
+              {item.product.category ? item.product.category.name : ""}
+            </div>
+            <div
+              onClick={handleShow}
+              className="d-flex pt-2 "
+              style={{ cursor: "pointer" }}
+            >
               <img src={deleteicon} alt="" width="20px" height="24px" />
               <div className="cat-text d-inline me-2">ازاله</div>
             </div>
@@ -49,26 +66,29 @@ const CartItem = ({item}) => {
         <Row className="justify-content-center mt-2">
           <Col sm="12" className=" d-flex flex-row justify-content-start">
             <div className="d-inline pt-2 cat-title">
-              {item.product?item.product.title : ""}
-
+              {item.product ? item.product.title : ""}
             </div>
-            <div className="d-inline pt-2 cat-rate me-2">{item.product?item.product.ratingsAverage : 0}</div>
+            <div className="d-inline pt-2 cat-rate me-2">
+              {item.product ? item.product.ratingsAverage : 0}
+            </div>
           </Col>
         </Row>
         <Row>
           <Col sm="12" className="mt-1">
             <div className="cat-text d-inline">الماركة :</div>
-            <div className="barnd-text d-inline mx-1">{item.product.brand?item.product.brand.name:""} </div>
+            <div className="barnd-text d-inline mx-1">
+              {item.product.brand ? item.product.brand.name : ""}{" "}
+            </div>
           </Col>
         </Row>
         <Row>
           <Col sm="12" className="mt-1 d-flex">
-            {
-              item.color === "" ? null : (<div
+            {item.color === "" ? null : (
+              <div
                 className="color ms-2 border"
-                style={{ backgroundColor: `${item.color}` }}></div>)
-            }
-
+                style={{ backgroundColor: `${item.color}` }}
+              ></div>
+            )}
           </Col>
         </Row>
 
@@ -83,14 +103,18 @@ const CartItem = ({item}) => {
                 type="number"
                 style={{ width: "60px", height: "40px" }}
               />
-              <Button onClick={handeleUpdateCart} className='btn btn-dark' >تطبيق</Button>
+              <Button onClick={handeleUpdateCart} className="btn btn-dark">
+                تطبيق
+              </Button>
             </div>
-            <div className="d-inline pt-2 barnd-text">{item.price?item.price: 0} جنية</div>
+            <div className="d-inline pt-2 barnd-text">
+              {item.price ? item.price : 0} جنية
+            </div>
           </Col>
         </Row>
       </div>
     </Col>
-    )
-}
+  );
+};
 
-export default CartItem
+export default CartItem;
