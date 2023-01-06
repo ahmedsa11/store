5f16124a6de2414a5bea905688bedcc02471783c
ahmedsa11 <ahmedsa122333@gmail.com>
2023-01-06 19:52:27 +0200

    finalllll

--- src/Api/baseUrl.js
+++ src/Api/baseUrl.js
@@ -5,6 +5,6 @@ import axios from "axios";
 //       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzOThhOGQ4ODI1NTlhNTk5NzlkYjZmOSIsImlhdCI6MTY3MTY0NzA5MCwiZXhwIjoxNjc5NDIzMDkwfQ.84FzWxaizoZ7kDrZ-jhiRgtoCrwZhcY4f9y1DIgYIak",
 //   },
 // };
-const baseUrl = axios.create({ baseURL: "http://127.0.0.1:8000" });
+const baseUrl = axios.create({ baseURL: "https://combative-plum-blackbuck.cyclic.app" });
 export default baseUrl;
  
